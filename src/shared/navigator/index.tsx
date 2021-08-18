b3f318563be2d8706086551f2edf9b53ea8dccd1
Hersey Aguilar <herseyaguilar@outlook.com>
2021-08-18 11:01:58 -0600

    chore: update to react-navigation@6 and other dev dependencies

--- src/shared/navigator/index.tsx
+++ src/shared/navigator/index.tsx
@@ -1,15 +1,15 @@
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import React, { FC } from 'react';
 import HomeScreen from 'screens/home';
 import { Screens } from 'shared/constants/screens';
 
-const Stack = createStackNavigator();
+const Stack = createNativeStackNavigator();
 
 const Navigator: FC = () => {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName={Screens.HOME} headerMode="none">
+      <Stack.Navigator initialRouteName={Screens.HOME} screenOptions={{ headerShown: false }}>
         <Stack.Screen name={Screens.HOME} component={HomeScreen} />
       </Stack.Navigator>
     </NavigationContainer>
